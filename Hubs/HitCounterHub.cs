ee2fbd170952c30c7e8c4285dc51c009e4ee3ad0
lightfromthesky <iamvidyaprakash@gmail.com>
2016-12-01 14:25:36 -0700

    First Pass

---
+++ Hubs/HitCounterHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace WebSignalR.Hubs
+{
+    [HubName("HitCounter")]
+    public class HitCounterHub: Hub
+    {
+        private static int _hitCounter = 0;
+        public override Task OnConnected()
+        {
+            _hitCounter += 1;
+            base.Clients.All.hitReceived(_hitCounter);
+            return base.OnConnected();
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            _hitCounter -= 1;
+            base.Clients.All.hitReceived(_hitCounter);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ Hubs/MoveShape.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace WebSignalR.Hubs
+{
+    [HubName("moveShape")]
+    public class MoveShape: Hub
+    {
+        public void moveShape(double x, double y)
+        {
+            Clients.Others.shapeMoved(x,y);
+        }
+    }
+}
